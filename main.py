e671d3638099f24a42947517cb068425c986d30f
ikeeyigsys5575 <58853620+ikeeyigsys5575@users.noreply.github.com>
2024-12-22 23:53:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,59 @@
+from time import sleep
+
 import requests
+import RPi.GPIO as GPIO
+import json
+
+GPIO.setmode(GPIO.BCM)
 
 url = "https://mcbroken.com/markers.json"
 
 
+status = []
+
+
+
 response = requests.get(url)
 
-if response.status_code == 200:
-    print("Received!")
-    print(response.json())
-else:
-    print(f"Failed to retrieve data. Status code: {response.status_code}")
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+targetAddresses = config['addresses']
+gpioBroken = config['gpioBroken']
+gpioGood = config['gpioWorking']
+
+def refresh():
+    if response.status_code == 200:
+        status.clear()
+        for address in targetAddresses:
+            data = response.json();
+            featuresArray = data['features']
+            for feature in featuresArray:
+                propertiesArray = feature['properties']
+                if (propertiesArray['street'] == address):
+                    status.append(propertiesArray['dot'])
+
+    for gpio in gpioBroken:
+        GPIO.setup(gpio, GPIO.OUT)
+
+    for gpio in gpioGood:
+        GPIO.setup(gpio, GPIO.OUT)
+
+    for i in range(len(status)):
+        if (status[i] == "broken"):
+            GPIO.output(gpioBroken[i], GPIO.HIGH)
+            GPIO.output(gpioGood[i], GPIO.LOW)
+        elif (status[i] == "working"):
+            GPIO.output(gpioBroken[i], GPIO.LOW)
+            GPIO.output(gpioGood[i], GPIO.HIGH)
+        else:
+            GPIO.output(gpioBroken[i], GPIO.LOW)
+            GPIO.output(gpioGood[i], GPIO.LOW)
+
+    print(status)
+
+while True:
+    refresh()
+    sleep(600)
+GPIO.cleanup()
+
